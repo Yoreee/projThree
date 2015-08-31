4ee313676d1905fbdae0789f6f07806e56a3cc01
David Nash <david.allyxanddar.nash@gmail.com>
2015-08-31 11:32:58 -0400

    api key read in from env var

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,17 +5,17 @@ class EventsController < ApplicationController
 	end
 
 	def search_location
-		@response = HTTParty.get("http://api.jambase.com/events?zipCode=" + params[:zip] + "&page=0&api_key=XXX&o=json")
+		@response = HTTParty.get("http://api.jambase.com/events?zipCode=" + params[:zip] + "&page=0&api_key=" + ENV["JAMBASE_VAR"] + "&o=json")
 		render :show
 	end
 
 	def search_artist
-		@response = HTTParty.get("http://api.jambase.com/artists?name=" + params[:name].gsub(/ /, '+') + "&page=0&api_key=XXX")
+		@response = HTTParty.get("http://api.jambase.com/artists?name=" + params[:name].gsub(/ /, '+') + "&page=0&api_key=" + ENV["JAMBASE_VAR"])
 		render :artists
 	end
 
 	def search_id
-		@response = HTTParty.get("http://api.jambase.com/events?artistId=" + params[:id] + "&page=0&api_key=XXX")
+		@response = HTTParty.get("http://api.jambase.com/events?artistId=" + params[:id] + "&page=0&api_key=" + ENV["JAMBASE_VAR"])
 		render :show
 	end
 
