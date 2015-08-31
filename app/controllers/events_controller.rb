c812a0b18b859250a6fac21e00e45796f70c5155
David Nash <david.allyxanddar.nash@gmail.com>
2015-08-31 10:44:49 -0400

    boop

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,17 +5,17 @@ class EventsController < ApplicationController
 	end
 
 	def search_location
-		@response = HTTParty.get("http://api.jambase.com/events?zipCode=" + params[:zip] + "&page=0&api_key=XXX&o=json")
+		@response = HTTParty.get("http://api.jambase.com/events?zipCode=" + params[:zip] + "&page=0&api_key=x964eyu32g34mdftwybccx64&o=json")
 		render :show
 	end
 
 	def search_artist
-		@response = HTTParty.get("http://api.jambase.com/artists?name=" + params[:name].gsub(/ /, '+') + "&page=0&api_key=XXX")
+		@response = HTTParty.get("http://api.jambase.com/artists?name=" + params[:name].gsub(/ /, '+') + "&page=0&api_key=x964eyu32g34mdftwybccx64")
 		render :artists
 	end
 
 	def search_id
-		@response = HTTParty.get("http://api.jambase.com/events?artistId=" + params[:id] + "&page=0&api_key=XXX")
+		@response = HTTParty.get("http://api.jambase.com/events?artistId=" + params[:id] + "&page=0&api_key=x964eyu32g34mdftwybccx64")
 		render :show
 	end
 
