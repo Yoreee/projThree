2c4d922444ada8a7e1237b88a1356246d1a05a40
Riaz Bacchus <riaz.bacchus@me.com>
2015-09-01 09:34:36 -0400

    fixed database issue, added redirect after creating event, fixed event create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,5 @@
+require('json')
+
 class EventsController < ApplicationController
 
 	def index
@@ -16,7 +18,33 @@ class EventsController < ApplicationController
 
 	def search_id
 		@response = HTTParty.get("http://api.jambase.com/events?artistId=" + params[:id] + "&page=0&api_key=" + ENV["JAMBASE_VAR"])
+		@response["Events"].map! do |event|
+			# event = JSON.parse(event)
+			event_attrs = {
+				event_date: event["Date"],
+				venue_name: event["Venue"]["Name"],
+				venue_city: event["Venue"]["City"],
+				venue_state: event["Venue"]["State"],
+				artists: event["Artists"],
+				ticket_url: event["TicketUrl"]
+			}
+
+			Event.new(event_attrs) 
+		end
+
+		puts @response["Events"][0].inspect
+
 		render :show
 	end
 
+	def create
+		@events = Event.create(event_params)
+		redirect_to :back
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:event_date, :venue_name, :venue_city, :venue_state, :artists, :ticket_url)
+	end
 end
