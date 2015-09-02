74c97c34ec5417f5e8ff68ef2bcbead0a0f60ab9
katerina burshteyn <katburst1@yahoo.com>
2015-09-02 15:37:01 -0400

    latest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to root_path
+			redirect_to user_path(user.id)
 		else
 			flash[:notice] = "Login failed."
 			redirect_to root_path
