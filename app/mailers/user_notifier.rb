c5cdaf057a36154df35f836f9eb7880bba5c8abe
Heather Kemp <hkemp77@gmail.com>
2015-09-03 08:10:16 -0400

    Final styles and mailer fixes

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -2,6 +2,6 @@ class UserNotifier < ApplicationMailer
     def send_signup_email(user)
         @user = user
         mail( :to => @user.email,
-        :subject => 'Thanks for signing up for Concert App' )
+        :subject => 'Thanks for signing up for bandIT' )
     end
 end
